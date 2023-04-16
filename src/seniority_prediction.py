7fafb2fcd0b3fcddc12e3668c1c83b9832f44e8b
OmerKorenAF <omerkoren@autofleet.io>
2023-04-16 20:28:30 +0300

    removed redundant config

--- src/seniority_prediction.py
+++ src/seniority_prediction.py
@@ -1,4 +1,22 @@
-import seniority_prediction_config as config
+# If title includes one of the substrings (per management level) we will mark it as "in management level
+relevant_title_parts_per_management_level = {
+    'c_level': [
+        'chief', 'ceo', 'cto', 'cpo', 'cso', 'coo', 'cfo', 'cmo', 'cio', 'cao', 'chro',
+        'president', 'global head'
+    ],
+    'vp_level': ['vp', 'vice president', 'vice-president'],
+    'director_level': ['director', 'dir.', 'directeur'],
+    'manager_level': ['manager', 'team lead', 'team-lead'],
+}
+
+# If title includes one of the substrings (per management level) we will mark it as "not in management level-
+# will be applied after the logic from above.
+substrings_to_exclude_per_management_level = {
+    'c_level': ['coordin', 'director', 'vice'],
+    'vp_level': [],
+    'director_level': [],
+    'manager_level': [],
+}
 
 
 def check_if_title_contains_keyword(title, list_of_keywords):
